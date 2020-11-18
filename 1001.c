ce1d39c70252bb337f149a9173a138b3ff0bc092
KeiTaylor0606 <mvemtae5757@outlook.jp>
2020-11-18 20:21:04 +0900

    10-1

---
+++ 1001.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void adjust_point(int *n){
+
+    if(*n < 0){
+        *n = 0;
+    }
+    else if(*n > 100){
+        *n = 100;
+    }
+
+}
+
+int main(void){
+    int n;
+    printf("n = ");
+    scanf("%d", &n);
+
+    adjust_point(&n);
+    printf("%d\n", n);
+    
+    return 0;
+}
